58769f05505df3bab2bce2fa40c13649c749ef35
Guillermo <pasante@easymetering.com>
2023-03-06 17:27:55 -0500

    FIX: pc issue

--- src/ezm_cpu.v
+++ src/ezm_cpu.v
@@ -12,20 +12,15 @@ ezm_cpu cpu_top(
 endmodule
 
 module ezm_cpu(in_i,clk,rst,out_o);
-input [5:0] in_i;
-input clk,rst;
-output wire [7:0]out_o;
-
-
+input wire[5:0] in_i;
+input wire clk,rst;
+output reg [7:0]out_o;
 reg [7:0] c=0,pc=0;
 reg [7:0] bank[7:0];
-reg bflag,reset,state=0;
-
-reg [2:0] instruction;
-
-
+reg reset,state=0;
+reg [2:0] instruction = 2'b00;
+reg [5:0] instruction_buffer;
 integer i; 
-reg instruction_flag;
 
 
 always @(rst)begin 
@@ -34,69 +29,82 @@ always @(rst)begin
      reset <=0;
 end
 
-always @(posedge clk)begin 
+always @(posedge clk)begin
+     if (reset)begin
+         state<=0;
+     end 
+     else begin
+          state <= ~state;
+          end      
+end
+
+always @(state)begin 
      if (reset)begin 
      for (i=0;i<8;i=i+1)begin
-     bank[i] <= 8'b0;
+     bank[i] = 8'b0;
      end  
-     c <= 8'b0;
-     pc<= 8'b0;
-     state<=0;
+     c = 8'b0;
+     pc= 8'b0;
+
      end 
      else begin
 
      case (state)
      0:begin
-     pc <= pc + 1'b1;
+     
+     instruction_buffer = in_i; 
 
-     casex (in_i) 
+     casex (instruction_buffer) 
+          6'b000000:  instruction=3'b000;  //nop this is important 
+          
           6'b1?????: 
-          instruction<=3'b001; //load         
+          instruction=3'b001;  //load         
           
           6'b011???:
-          instruction<=3'b010; //conditional branch
+          instruction=3'b010; //conditional branch
           
           6'b001???:
-          instruction<=3'b011;//stra
+          instruction=3'b011;//stra
 
           6'b010???:
-          instruction<=3'b100;//add
+          instruction=3'b100;//add
           
           6'b000001: 
-          instruction<=3'b101;
-          default:instruction<=3'b000;
+          instruction=3'b101; //not
+          
+          default:instruction=3'b000;  //nop 
      endcase
-     state<=1'b1;
+     out_o= c;
      end 
 
      1: begin 
-
-     bflag<=0;
+ 
      case (instruction) 
           3'b001:begin 
-          c <= {{3{in_i[4]}},in_i[4:0]}; //load
+          c = {{3{instruction_buffer[4]}},instruction_buffer[4:0]}; //load
+          pc=pc+8'd1;
           end
           3'b010:begin 
-          if (bank[in_i[2:0]]>c) begin // value in register greater than accumulator
-          pc <= pc - c;
+          if (bank[instruction_buffer[2:0]]>c) begin // value in register greater than accumulator
+          pc = pc - c;
           end 
           end //conditional branch
           3'b011: begin 
-               bank[in_i[2:0]] <= c;
+            bank[instruction_buffer[2:0]] = c;
+            pc=pc+8'd1;
           end  
           3'b100: begin 
-          c <= (bank[in_i[2:0]] + c);
+            c = (bank[instruction_buffer[2:0]] + c);
+            pc=pc+8'd1;
           end//add
-          3'b101: 
-          c<=~c;//negate 
-          default:;
+          3'b101:begin
+            c = ~c; pc=pc+8'd1; end //negate 
+          default:pc =pc+ 8'd1;
      endcase
-
-     state<=1'b0; 
+     out_o= pc; 
      end
 endcase
 end
 end
-assign out_o = state ? {c}:{pc};
 
 endmodule
